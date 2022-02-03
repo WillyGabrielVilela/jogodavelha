7e33acfc424f8c6c132c715f577fcbb6a81a8d1e
WillyGabrielVilela <willygabrielnoval@gmail.com>
2022-02-03 13:29:32 -0300

    Lets Rock

---
+++ interface.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick);
+    })
+
+})
+
+function handleClick(event) {
+
+
+    let square = event.target;
+    let postion = square.id;
+
+    if (handleMove(postion)) {
+
+        setTimeout(() => {
+            alert("O Jogo Acabou - Parabéns jogador " + playerTime )
+
+        }, 10);
+    }
+    updateSquare(postion);
+}
+
+function updateSquare(postion) {
+    let square = document.getElementById(postion.toString());
+    let symbol = board[postion];
+    square.innerHTML = square.innerHTML = `<div class='${symbol}'></div>`
+}
+
+
+
